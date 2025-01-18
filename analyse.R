883e0960e54e7b57bac1c475fc5ba3cc83aff967
lrblst <195580940+lrblst@users.noreply.github.com>
2025-01-18 15:03:01 -0500

    Revert "ajout de transparence"
    This reverts commit 81700d712bf8fbcf273deb547cbfe2ac7449a33e.

--- analyse.R
+++ analyse.R
@@ -4,6 +4,6 @@ library(ratdat)
 
 #graphique
 ggplot(data = complete_old, aes(x = weight, y = hindfoot_length)) +
-  geom_point(color = "red", alpha = 0.2)
+  geom_point(color = "red")
 
 #git add analyse.R / git commit -m "ajout de la couleur"
