81700d712bf8fbcf273deb547cbfe2ac7449a33e
lrblst <195580940+lrblst@users.noreply.github.com>
2025-01-18 14:56:12 -0500

    ajout de transparence

--- analyse.R
+++ analyse.R
@@ -4,6 +4,6 @@ library(ratdat)
 
 #graphique
 ggplot(data = complete_old, aes(x = weight, y = hindfoot_length)) +
-  geom_point(color = "red")
+  geom_point(color = "red", alpha = 0.2)
 
 #git add analyse.R / git commit -m "ajout de la couleur"
